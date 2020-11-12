9db9160d410290a0a01b61de4a939e82a8c4bf4e
Julien Rousseau <julien.rousseau@apside-groupe.com>
2020-11-12 15:40:17 +0100

    SCSS import

--- assets/app.js
+++ assets/app.js
@@ -6,9 +6,11 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+import './styles/app.scss';
+import './styles/answerProvider.scss';
 
-// Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
-// import $ from 'jquery';
 
-console.log('Hello Webpack Encore! Edit me in assets/app.js');
+// Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
+const $ = require('jquery');
+global.$ = global.jQuery = $;
+require('bootstrap')
